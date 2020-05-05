1237f1f865e61e04fef23f6d59b67a93a63ba66a
acerjak <62491401+acerjak@users.noreply.github.com>
2020-05-05 16:49:28 -0700

    attempted UV Index

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ $('#cityBtn').on('click', event => {
     currentWeatherElem.addClass('card')
     //adding width style to newly created current weather div
     currentWeatherElem.attr('style', 'width:70rem',)
-    currentWeatherElem.attr('style', 'padding:1rem',)
+    currentWeatherElem.attr('style', 'padding:1rem',)   
     //writing HTML into the newly created current weather div
     currentWeatherElem.html(`
         <h5 class="card-title">
@@ -58,11 +58,26 @@ $('#cityBtn').on('click', event => {
                 <li class="list-group-item">
                 Wind Speed: ${current.wind.speed} MPH
                 </li>
-                <li class="list-group-item">
+                <li class="list-group-item" value="${index.value}" id="uv">
                 UV Index: ${index.value}
                 </li>
             </ul>`
-        )
+        ) 
+    //define variable for uv
+    let uv = $('#uv');
+    // add green color btn to UV Index
+if (uv.value < 3) {
+    console.log('green')
+    uv.addClass('success');
+    //add yellow color btn to UV Index
+} else if (uv.value < 7) {
+    console.log('yellow')
+    uv.addClass('warning');
+    //add red color btn to UV index
+} else {
+    console.log('red')
+    uv.addClass('danger');
+}   
     //create div for 5 day forecast
     let fiveDayForecastElem = $('<div>')
     //add classes to this div
@@ -74,56 +89,58 @@ $('#cityBtn').on('click', event => {
         <div class="col-md-9">
         <h4 class="card-title">5-Day Forecast:</h4>
         <div class="row">
-        <div class="card-body">
-            <h5 class="card-title">${forecast.list[2].dt_txt.split(" ")[0]}</h5>
-                <img src="http://openweathermap.org/img/wn/${forecast.list[0].weather[0].icon}@2x.png"
+            <div class="card bg-primary">
+                <h5 class="card-title">${forecast.list[2].dt_txt.split(" ")[0]}</h5>
+                    <img src="http://openweathermap.org/img/wn/${forecast.list[0].weather[0].icon}@2x.png"
+                    attr="SameSite" alt="${current.name}">
+                    <p class="card-text">
+                    Temp: ${forecast.list[0].main.temp}°F
+                    <br>
+                    Humidity: ${forecast.list[0].main.humidity}%
+                    </p>
+            </div>
+            <div class="card bg-primary">
+                <h5 class="card-title">${forecast.list[8].dt_txt.split(" ")[0]}</h5>
+                <img src="http://openweathermap.org/img/wn/${forecast.list[8].weather[0].icon}@2x.png"
                 attr="SameSite" alt="${current.name}">
                 <p class="card-text">
-                Temp: ${forecast.list[0].main.temp}°F
+                Temp: ${forecast.list[8].main.temp}°F
                 <br>
-                Humidity: ${forecast.list[0].main.humidity}%
+                Humidity: ${forecast.list[8].main.humidity}%
                 </p>
+            </div>
+            <div class="card bg-primary">
+                <h5 class="card-title">${forecast.list[16].dt_txt.split(" ")[0]}</h5>
+                    <img src="http://openweathermap.org/img/wn/${forecast.list[16].weather[0].icon}@2x.png"
+                    attr="SameSite" alt="${current.name}">
+                    <p class="card-text">
+                    Temp: ${forecast.list[16].main.temp}°F
+                    <br>
+                    Humidity: ${forecast.list[16].main.humidity}%
+                    </p>
+            </div>
+            <div class="card bg-primary">
+                <h5 class="card-title">${forecast.list[24].dt_txt.split(" ")[0]}</h5>
+                    <img src="http://openweathermap.org/img/wn/${forecast.list[24].weather[0].icon}@2x.png"
+                    attr="SameSite" alt="${current.name}">
+                    <p class="card-text">
+                    Temp: ${forecast.list[24].main.temp}°F
+                    <br>
+                    Humidity: ${forecast.list[24].main.humidity}%
+                    </p>
+            </div>
+            <div class="card bg-primary">
+                    <h5 class="card-title">${forecast.list[32].dt_txt.split(" ")[0]}</h5>
+                    <img src="http://openweathermap.org/img/wn/${forecast.list[32].weather[0].icon}@2x.png"
+                    attr="SameSite" alt="${current.name}">
+                    <p class="card-text">
+                    Temp: ${forecast.list[32].main.temp}°F
+                    <br>
+                    Humidity: ${forecast.list[32].main.humidity}%
+                    </p>
+            </div>
         </div>
-        <div class="card-body">
-        <h5 class="card-title">${forecast.list[8].dt_txt.split(" ")[0]}</h5>
-            <img src="http://openweathermap.org/img/wn/${forecast.list[8].weather[0].icon}@2x.png"
-            attr="SameSite" alt="${current.name}">
-            <p class="card-text">
-            Temp: ${forecast.list[8].main.temp}°F
-            <br>
-            Humidity: ${forecast.list[8].main.humidity}%
-            </p>
         </div>
-        <div class="card-body">
-        <h5 class="card-title">${forecast.list[16].dt_txt.split(" ")[0]}</h5>
-            <img src="http://openweathermap.org/img/wn/${forecast.list[16].weather[0].icon}@2x.png"
-            attr="SameSite" alt="${current.name}">
-            <p class="card-text">
-            Temp: ${forecast.list[16].main.temp}°F
-            <br>
-            Humidity: ${forecast.list[16].main.humidity}%
-            </p>
-        </div>
-        <div class="card-body">
-        <h5 class="card-title">${forecast.list[24].dt_txt.split(" ")[0]}</h5>
-            <img src="http://openweathermap.org/img/wn/${forecast.list[24].weather[0].icon}@2x.png"
-            attr="SameSite" alt="${current.name}">
-            <p class="card-text">
-            Temp: ${forecast.list[24].main.temp}°F
-            <br>
-            Humidity: ${forecast.list[24].main.humidity}%
-            </p>
-        </div>
-        <div class="card-body">
-        <h5 class="card-title">${forecast.list[32].dt_txt.split(" ")[0]}</h5>
-            <img src="http://openweathermap.org/img/wn/${forecast.list[32].weather[0].icon}@2x.png"
-            attr="SameSite" alt="${current.name}">
-            <p class="card-text">
-            Temp: ${forecast.list[32].main.temp}°F
-            <br>
-            Humidity: ${forecast.list[32].main.humidity}%
-            </p>
-        </div></div></div>
     `)
     $('#currentForecast').append(currentWeatherElem)
     $('#futureForecast').append(fiveDayForecastElem)
@@ -132,6 +149,7 @@ $('#cityBtn').on('click', event => {
     console.log(getHistory)
     window.localStorage.setItem("searchHistory", JSON.stringify(getHistory));
     makeItem($('#cityInput').val());
+
 })
 //make list clickable and searchable
     $('.searchHistory').on('click', 'li', function() {
@@ -194,7 +212,7 @@ $('#cityBtn').on('click', event => {
         <div class="col-md-9">
         <h4 class="card-title">5-Day Forecast:</h4>
         <div class="row">
-        <div class="card-body">
+        <div class="card bg-primary">
             <h5 class="card-title">${forecast.list[2].dt_txt.split(" ")[0]}</h5>
                 <img src="http://openweathermap.org/img/wn/${forecast.list[0].weather[0].icon}@2x.png"
                 attr="SameSite" alt="${current.name}">
@@ -204,7 +222,7 @@ $('#cityBtn').on('click', event => {
                 Humidity: ${forecast.list[0].main.humidity}%
                 </p>
         </div>
-        <div class="card-body">
+        <div class="card bg-primary">
         <h5 class="card-title">${forecast.list[8].dt_txt.split(" ")[0]}</h5>
             <img src="http://openweathermap.org/img/wn/${forecast.list[8].weather[0].icon}@2x.png"
             attr="SameSite" alt="${current.name}">
@@ -214,7 +232,7 @@ $('#cityBtn').on('click', event => {
             Humidity: ${forecast.list[8].main.humidity}%
             </p>
         </div>
-        <div class="card-body">
+        <div class="card bg-primary">
         <h5 class="card-title">${forecast.list[16].dt_txt.split(" ")[0]}</h5>
             <img src="http://openweathermap.org/img/wn/${forecast.list[16].weather[0].icon}@2x.png"
             attr="SameSite" alt="${current.name}">
@@ -224,7 +242,7 @@ $('#cityBtn').on('click', event => {
             Humidity: ${forecast.list[16].main.humidity}%
             </p>
         </div>
-        <div class="card-body">
+        <div class="card bg-primary">
         <h5 class="card-title">${forecast.list[24].dt_txt.split(" ")[0]}</h5>
             <img src="http://openweathermap.org/img/wn/${forecast.list[24].weather[0].icon}@2x.png"
             attr="SameSite" alt="${current.name}">
@@ -234,7 +252,7 @@ $('#cityBtn').on('click', event => {
             Humidity: ${forecast.list[24].main.humidity}%
             </p>
         </div>
-        <div class="card-body">
+        <div class="card bg-primary">
         <h5 class="card-title">${forecast.list[32].dt_txt.split(" ")[0]}</h5>
             <img src="http://openweathermap.org/img/wn/${forecast.list[32].weather[0].icon}@2x.png"
             attr="SameSite" alt="${current.name}">
@@ -243,14 +261,9 @@ $('#cityBtn').on('click', event => {
             <br>
             Humidity: ${forecast.list[32].main.humidity}%
             </p>
-        </div></div></div>
+    </div></div></div>
     `)
     $('#currentForecast').append(currentWeatherElem)
     $('#futureForecast').append(fiveDayForecastElem)
     })})})
-//create history section and links for this search
-    // if (getHistory.indexOf(cityInput) === -1) {
-
-    // } 
-    // makeItem() 
 })
